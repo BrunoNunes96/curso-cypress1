d29c39ace16368c989b4a0e80bae3cab100dac56
BRUNO CESAR NUNES <34582524+BrunoNunes96@users.noreply.github.com>
2021-11-15 17:50:39 -0300

    Primeiro teste automatizado na obj

--- cypress/integration/teste.spec.js
+++ cypress/integration/teste.spec.js
@@ -1,17 +1,32 @@
 /// <reference types="cypress"/>
 
-describe('teste de login do facebook',()=>{
-    beforeEach(()=>{
+// titulo da analise/teste
+describe(' Validar seção Benefício',()=>{
+
+    // acessando link inicial da aplicação
+    before(()=>{
         
+        cy.visit('https://afiliados.rdstation.com/affiliates/login.php#login')
     })
-    it('Testando widgets',()=>{
-        cy.visit('https://resdigitaisstg.wpengine.com/')
-        cy.get('#home-sidebar > .widget-signup > .widget-content > #rdm_test > #email').type("bruno_nunes92@outlook.com")
-        cy.get('#home-sidebar > .widget-signup > .widget-content > #rdm_test > .btn-base').click()   
-        cy.visit('https://app.rdstation.com.br/signup?trial_origin=rds_blog&_ga=2.11434484.1358757707.1636565568-378108872.1636565568')
-       // it(' pagina de teste do RD Station',()=>{
-       //     cy.visit('https://app.rdstation.com.br/signup?trial_origin=rds_blog&_ga=2.11434484.1358757707.1636565568-378108872.1636565568')
+
+    // validando pré-requisito da pagina e logando com usuario afiliado
+    it(' Pré- Requisito: Logar com usuario afiliado',()=>{
+
+        cy.get(':nth-child(1) > .FormField > .FormFieldInputContainer > .FormFieldInput > .TextBoxContainer > .TextBox').type('vinilaureto@gmail.com')
+        cy.get(':nth-child(2) > .FormField > .FormFieldInputContainer > .FormFieldInput > .TextBoxContainer > .TextBox').type('teste123')
+        cy.get('.ImLeButtonMainContent > .ButtonText').click()
         })
 
+    it('Deve validar o texto " Benefício para seus indicados" ',()=>{
+
+        // acessando seção e subseção pelos botos do menu
+        cy.get(':nth-child(4) > .MenuBoxLeft > .MenuBoxRight > .MenuBoxContent > .MenuBoxLabel > .SectionMenuEntry > .MenuEntry').click()
+        cy.get('.MenuBox-open > .MenuBoxLeft > .MenuBoxRight > .MenuBoxContent > .SubMenuSection > .SubMenuEntries').click()
+
+
+        // validando os campos de texto
+        cy.get('.panel_content > div > :nth-child(1)').should('contain', 'Todo lead que se tornar um cliente RD Station Marketing através de seu link de afiliado ganha um curso da RD University.')
+        cy.get('.panel_content > div > :nth-child(2)').should('contain', 'Você não precisa fazer nada para participar: após assinar o RD Station Marketing, seu indicado receberá um e-mail com as instruções para escolher o curso.')
     })      
 
+})  
