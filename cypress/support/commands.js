3ed83b82610644b4700414bb1921711db936b579
BRUNO CESAR NUNES <34582524+BrunoNunes96@users.noreply.github.com>
2021-11-17 15:30:33 -0300

    Alteração do repositorio

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login',(email, pw) =>{
+    cy.visit()
+    cy.get('#buttonEmail').type('bruno@hotmail.com')
+    cy.get('#buttonPw').type('1234')
+})
